aec51d7ea8ca9ed3cb5631af0ba9be0865e2c9d3
W8jonas <jonashenrique989@gmail.com>
2024-11-01 00:19:13 -0300

    Altera de javascript para python

--- createWallet.js
+++
@@ -1,7 +0,0 @@
-const StellarSdk = require("@stellar/stellar-sdk")
-
-const pair = StellarSdk.Keypair.random()
-
-const [publicKey, privateKey] = [pair.publicKey(), pair.secret()]
-
-console.log(`Public key: ${publicKey}\nprivate key: ${privateKey}`)
